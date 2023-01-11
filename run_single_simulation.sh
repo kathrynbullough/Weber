12cc704084f603e74698fe4b5212cd89721a2571
Kathryn Bullough <kathryn.bullough@googlemail.com>
2023-01-11 16:05:02 +0000

    Create run_single_simulation.sh

---
+++ run_single_simulation.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      sex-limited p       sex-limited t       pref type   file name
+./fisherian.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher.csv
