4f2b228cf49acc65caaef1cee426cb55907e34a2
Bram Kuijper <bramkuijper@gmail.com>
2023-01-20 13:24:51 +0000

    adding bash file

--- run_single_simulation.sh
+++ run_single_simulation.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      sex-limited p       sex-limited t       pref type   file name
-./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher.csv
+./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher1.csv &
+./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher2.csv &
+./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher3.csv &
+./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher4.csv &
