a4c6cf8dd763ab0beb8bf099d8438cccad2026b6
Bullough <kb574@rstudio01n.isad.isadroot.ex.ac.uk>
2023-01-20 14:34:38 +0000

    fix

--- make_that_bash_file.r
+++ make_that_bash_file.r
@@ -5,6 +5,14 @@ parameter_object$a <- 1.0
 parameter_object$b <- 0.01
 parameter_object$c <- seq(0,0.2,0.01)
 parameter_object$biast <- 0.05
+parameter_object$mu_p <- 0.01
+parameter_object$mu_t <- 0.01
+parameter_object$sdmu_p <- 0.05
+parameter_object$sdmu_t <- 0.05
+parameter_object$sex-limited p <- 1.0
+parameter_object$sex-limited t <- 1.0
+parameter_object$pref type <- 0
+parameter_object$web <- 1
 
 #purlease continue adding all the parameters, except for the output file
 # (this will be done by the function itself)
--- run_single_simulation.sh
+++ run_single_simulation.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 # exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      sex-limited p       sex-limited t       pref type       web   file name
 ./Weber.exe   1.0     0.5    0.5     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0                  1  output_fisher.csv
-=======
