018098e87bd77ea7b3ce398b8bc34b80256d7eb2
Bullough <kb574@rstudio01n.isad.isadroot.ex.ac.uk>
2023-01-20 14:16:41 +0000

    make one sim

--- run_single_simulation.sh
+++ run_single_simulation.sh
@@ -4,9 +4,3 @@
 # exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      sex-limited p       sex-limited t       pref type       web   file name
 ./Weber.exe   1.0     0.5    0.5     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0                  1  output_fisher.csv
 =======
-# exe name      a       b       c       biast       mu_p    mu_t    sdmu_p      sdmu_t      sex-limited p       sex-limited t       pref type   file name
-./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher1.csv &
-./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher2.csv &
-./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher3.csv &
-./Weber.exe   1.0     0.01    0.2     0.05        0.01    0.01    0.05        0.05        1.0                 1.0                 0           output_fisher4.csv &
->>>>>>> 4f2b228cf49acc65caaef1cee426cb55907e34a2
