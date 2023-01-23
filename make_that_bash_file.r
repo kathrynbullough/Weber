ec93fb02e65b835c102ceab16b49dd5cf6fbece5
Bullough <kb574@rstudio01n.isad.isadroot.ex.ac.uk>
2023-01-23 13:55:16 +0000

    fix

--- make_that_bash_file.r
+++ make_that_bash_file.r
@@ -19,3 +19,5 @@ parameter_object$web <- 1
 
 # please be sensible with the amount of parameters
 
+make_batch_file(parameter_list=parameter_object
+                     ,executable_path="./my_simulation.exe")
