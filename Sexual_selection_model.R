984abbb90b0f5b5855406061e02cc2b649ec850d
Bram Kuijper <bramkuijper@gmail.com>
2023-02-02 11:29:05 +0000

    adding a plot to multiplex your files

--- Sexual_selection_model.R
+++ Sexual_selection_model.R
@@ -1,4 +1,65 @@
-data <- read.delim("output_fisher_TEST.csv", sep=";", header=T)
+#!/usr/bin/env Rscript
+# plots everything from a single sim 
+
+library("tidyverse")
+library("patchwork")
+
+# obtain the final line of the actual simulation data
+# of each file
+get_last_line_number <- function(file_name)
+{
+    # get all the lines
+    all_lines <- readLines(con=file_name)
+    
+    # obtain number of lines
+    nlines <- length(all_lines)
+    
+    # search for last line from the end
+    # (coz faster)
+    for (line_idx in seq(nlines,1,-1))
+    {
+        # find the line that starts with a number using grep()
+        # if grep returns something, anything, its length > 0
+        # hence the return the index of that line and we are done
+        if (length(grep(pattern="^\\d",all_lines[[line_idx]]))>0)
+        {
+            return(line_idx)
+        }
+    }
+    
+    # no line means no data hence quit
+    stop("no line found")
+}
+
+
+file.name <- commandArgs(trailingOnly=T)[[1]]
+# get the last line
+last.line <- get_last_line_number(file_name = file.name)
+
+# read stuff into a Hadleyverse tibble
+sim_data <- read_delim(file=file.name, 
+        delim=";", 
+        n_max=last.line - 1)
+
+print(head(sim_data))
+
+plot_mean_t <- ggplot(data=sim_data
+        ,mappping=aes(x=generation
+                ,y=mean_t)) + 
+    geom_line(mapping=aes(x=generation,y=mean_t))
+
+## plot mean p in a separate graph
+plot_mean_p <- ggplot(data=sim_data
+        ,mappping=aes(x=generation
+                ,y=mean_p)) + geom_line(mapping=aes(x=generation,y=mean_p))
+
+plot_mean_t / plot_mean_p
+
+ggsave(file=paste0("graph_",basename(file.name),".pdf"))
+    
+stop()
+
+
 plot(data$generation,data$mean_t)
 plot(data$generation,data$mean_p)
 plot(data$generation,data$surviving_males)
