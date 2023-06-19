270924a05b1a1324bdcfcbcc570a9ededc068e77
Kathryn Bullough <kathryn.bullough@googlemail.com>
2023-06-19 13:29:48 +0100

    Add files via upload

---
+++ multidimensional/summary.r
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+library("simulation.utils")
+library("readr")
+
+all.bloody.data <- summarize.sims(simulations_path="."
+        ,simulation_file_pattern="output_\\d"
+        ,parameter_start_pattern="^type"
+        ,data_end_pattern="^$"
+        )
+
+# some shizzle like this to get the file on disk
+write_delim(file="sims_output_fish.csv", x=all.bloody.data)
