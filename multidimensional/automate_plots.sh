38a0e3e09975b96cd0cabf3a3cb780eff7a4d21b
Kathryn Bullough <kathryn.bullough@googlemail.com>
2023-06-19 14:21:05 +0100

    Add files via upload

---
+++ multidimensional/automate_plots.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+
+# find all the output files that match this grub and then perform an Rscript on them
+# please change pattern at will to match what you are having
+find . -iname "output_*" -exec ./Sexual_selection_model.R {} \;
