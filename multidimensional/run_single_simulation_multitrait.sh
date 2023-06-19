846b8729b0abaf3a85860fecd81a378eb6c74484
Kathryn Bullough <kathryn.bullough@googlemail.com>
2023-06-19 11:08:06 +0100

    Update run_single_simulation_multitrait.sh

--- multidimensional/run_single_simulation_multitrait.sh
+++ multidimensional/run_single_simulation_multitrait.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # exe name    a       b          c     lambda     biast       mu_p    mu_t    sdmu_p    sdmu_t   sex-limited p       sex-limited t       pref type       web    init_t    init_p   gam   thet   file name
-./Weber.exe   1     0.001        0.5     0.5      0.9        0.05      0.05      0.05     0.05        1                    1                  0            0       0         3     2.0   0.5  output_multi.csv
+./Weber_multiple.exe   1     0.001        0.5     0.5      0.9        0.05      0.05      0.05     0.05        1                    1                  0            0       0         3     2.0   0.5  output_multi.csv
