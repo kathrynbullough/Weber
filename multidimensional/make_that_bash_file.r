a27961993060217f2fd32e42edd146a64820faaf
Bullough <kb574@rstudio02n.isad.isadroot.ex.ac.uk>
2023-07-03 09:54:41 +0100

    updated parameters

--- multidimensional/make_that_bash_file.r
+++ multidimensional/make_that_bash_file.r
@@ -9,17 +9,17 @@ parameter_object <- list()
 parameter_object$a <- 1    #Choice slope
 parameter_object$b <- 0.001      #Cost of preference
 parameter_object$c <- 0.5      #Cost of trait
-parameter_object$lambda <- 1
+parameter_object$lambda <- 1    #Keep as 1!!!
 parameter_object$biast1 <- 0.9  #Mutation bias
-parameter_object$biast2 <- 0.9
+parameter_object$biast2 <- 0.5
 parameter_object$mu_p1 <- 0.05
-parameter_object$mu_p2 <- 0.05
+parameter_object$mu_p2 <- 0.0
 parameter_object$mu_t1 <- 0.05
-parameter_object$mu_t2 <- 0.05
+parameter_object$mu_t2 <- 0.0
 parameter_object$sdmu_p1 <- 0.05
-parameter_object$sdmu_p2 <- 0.05
+parameter_object$sdmu_p2 <- 0.0
 parameter_object$sdmu_t1 <- 0.05
-parameter_object$sdmu_t2 <- 0.05
+parameter_object$sdmu_t2 <- 0.0
 parameter_object$sex_limited_p <- 1.0
 parameter_object$sex_limited_t <- 1.0
 parameter_object$pref_type <- 0
@@ -27,9 +27,9 @@ parameter_object$web <- 0
 parameter_object$init_t1 <- 0
 parameter_object$init_t2 <- 0
 parameter_object$init_p1 <- 3
-parameter_object$init_p2 <- 4
-parameter_object$gam <- 3
-parameter_object$thet <- 0.2
+parameter_object$init_p2 <- 0
+parameter_object$gam <- 2
+parameter_object$thet <- 1
 
 #purlease continue adding all the parameters, except for the output file
 # (this will be done by the function itself)
