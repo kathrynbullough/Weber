778028e042c4af3647bcdbd6ee83cf3bcb14c517
kb574 <kb574@rstudio03n.isad.isadroot.ex.ac.uk>
2024-07-05 09:21:30 +0100

    comments

--- multidimensional/make_that_bash_file.r
+++ multidimensional/make_that_bash_file.r
@@ -7,8 +7,8 @@ library("simulation.utils")
 parameter_object <- list()
 
 parameter_object$a <- 1    #Choice slope
-parameter_object$b <- seq(0,0.01,0.001) #0.001      #Cost of preference
-parameter_object$c <- seq(0,0.6,0.1) #0.2    #Cost of trait
+parameter_object$b <- 0.001 #0.001      #Cost of preference
+parameter_object$c <- 0.2 #0.2    #Cost of trait
 parameter_object$lambda <- 1    #Keep as 1!!!
 parameter_object$biast <- 0.9 #seq(0.5,1,0.02)       #Mutation bias
 parameter_object$mu_p <- 0.05
@@ -17,11 +17,11 @@ parameter_object$sdmu_p <- 0.05
 parameter_object$sdmu_t <- 0.05
 parameter_object$sex_limited_p <- 1.0
 parameter_object$sex_limited_t <- 1.0
-parameter_object$pref_type <- 0
+parameter_object$pref_type <- seq(0,3,3)
 parameter_object$init_t <- 0
 parameter_object$init_p <- 3
-parameter_object$gam <- 3 #2
-parameter_object$thet <- seq(0.1,0.9,0.1) #1
+parameter_object$gam <- 2 #2
+parameter_object$thet <- 1 #1
 parameter_object$ntrait <- 2
 
 #purlease continue adding all the parameters, except for the output file
@@ -30,7 +30,7 @@ parameter_object$ntrait <- 2
 # please be sensible with the amount of parameters
    
 make.batch.file(parameter_list=parameter_object
-                     ,executable_path="./Weber_multiple.exe",n_replicates=1)
+                     ,executable_path="./Weber_multiple.exe",n_replicates=5)
                      
                      
 #seq(0,0.5,0.1)
