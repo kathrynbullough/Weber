701bc7e75ab1536d999537b4a5825a0741b98dd2
Bram Kuijper <bramkuijper@gmail.com>
2024-11-19 16:25:12 +0000

    adding plotting capability for fisher

--- multidimensional_fisher/automate_plots.sh
+++ multidimensional_fisher/automate_plots.sh
@@ -2,4 +2,4 @@
 
 # find all the output files that match this grub and then perform an Rscript on them
 # please change pattern at will to match what you are having
-find . -iname "output_*" -exec ./Sexual_selection_model.R {} \;
+find . -iname "output_*" -exec ./plot_output.r {} \;
--- multidimensional_fisher/make_that_bash_file.r
+++ multidimensional_fisher/make_that_bash_file.r
@@ -7,8 +7,8 @@ library("simulation.utils")
 parameter_object <- list()
 
 parameter_object$a <- 1    #Choice slope
-parameter_object$b <- 0.001 #c(0.001,0.003,0.005)  #0.001      #Cost of preference
-parameter_object$c <- 0.2 #c(0.0,0.1,0.2,0.3,0.5)  #0.2    #Cost of trait
+parameter_object$b <- 0.00 #c(0.001,0.003,0.005)  #0.001      #Cost of preference
+parameter_object$c <- 0.0 #c(0.0,0.1,0.2,0.3,0.5)  #0.2    #Cost of trait
 parameter_object$lambda <- 1    #Keep as 1!!!
 parameter_object$biast <- 0.9 #seq(0.5,1,0.02)       #Mutation bias
 parameter_object$mu_p <- 0.05
@@ -17,12 +17,12 @@ parameter_object$sdmu_p <- 0.05
 parameter_object$sdmu_t <- 0.05
 parameter_object$sex_limited_p <- 1.0
 parameter_object$sex_limited_t <- 1.0
-parameter_object$pref_type <- c(0,3)
+parameter_object$pref_type <- c(3)
 parameter_object$init_t <- 0
 parameter_object$init_p <- 3
 parameter_object$gam <- 2  #c(0.1,1.0,2.0,3.0)  #2
 parameter_object$thet <- 1  #1
-parameter_object$ntrait <- 5
+parameter_object$ntrait <- 1
 
 #purlease continue adding all the parameters, except for the output file
 # (this will be done by the function itself)
@@ -30,7 +30,8 @@ parameter_object$ntrait <- 5
 # please be sensible with the amount of parameters
    
 make.batch.file(parameter_list=parameter_object
-                     ,executable_path="./Weber_multiple.exe",n_replicates=5)
+                     ,executable_path="./Weber_multiple.exe"
+                     ,n_replicates=5)
                      
                      
 #seq(0,0.5,0.1)
